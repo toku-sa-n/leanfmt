3be4adc729b9d1c5779cbed061e6ab41ce9a87a1
Hiroki Tokunaga <tokusan441@gmail.com>
2025-09-23 17:52:07 +0900

    feat: format `example`

--- Leanfmt/AST/Expr.lean
+++ Leanfmt/AST/Expr.lean
@@ -8,8 +8,8 @@ namespace Leanfmt.AST
 inductive Expr where
   | ident (id : Identifier) : Expr
   | num (value : String) : Expr
-  | str (value : String) : Expr
   | app (fn : Expr) (args : List Expr) : Expr
+  | binop (op : String) (left : Expr) (right : Expr) : Expr
   deriving Inhabited
 
 open Formattable Combinator in
@@ -19,12 +19,17 @@ where
   go : Expr → Printer Unit
     | .ident id => format id
     | .num value => text value
-    | .str value => text value
     | .app fn args => do
       go fn
       for arg in args do
         text " "
         go arg
+    | .binop op left right => do
+      go left
+      text " "
+      text op
+      text " "
+      go right
 
 open Lean (Syntax Name) in
 
@@ -38,8 +43,6 @@ partial def Expr.fromSyntax (stx : Syntax) : Except FormatError Expr := do
     return Expr.num val
   | Syntax.node _ `Lean.Parser.Term.num #[Syntax.atom _ val] =>
     return Expr.num val
-  | Syntax.node _ `Lean.Parser.Term.str #[Syntax.atom _ val] =>
-    return Expr.str val
   | Syntax.node _ `Lean.Parser.Term.app #[fn, Syntax.node _ `null args] =>
     let fn ← Expr.fromSyntax fn
     let mut parsedArgs : List Expr := []
@@ -47,6 +50,14 @@ partial def Expr.fromSyntax (stx : Syntax) : Except FormatError Expr := do
       let parsed ← Expr.fromSyntax arg
       parsedArgs := parsedArgs ++ [parsed]
     return Expr.app fn parsedArgs
+  | Syntax.node _ `«term_=_» #[left, _, right] =>
+    let leftExpr ← Expr.fromSyntax left
+    let rightExpr ← Expr.fromSyntax right
+    return Expr.binop "=" leftExpr rightExpr
+  | Syntax.node _ `«term_+_» #[left, _, right] =>
+    let leftExpr ← Expr.fromSyntax left
+    let rightExpr ← Expr.fromSyntax right
+    return Expr.binop "+" leftExpr rightExpr
   | _ =>
     throw (FormatError.unimplemented s!"expression: {stx.getKind}")
 
